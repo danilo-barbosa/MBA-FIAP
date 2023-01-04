66078a28ad35b2d1786860a42bdca6325b337013
Danilo Barbosa <danilo_barbosa@hotmail.com>
2023-01-03 22:12:32 -0300

    Version endpoint

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckerController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckerController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @RestController
 @RequestMapping("/")
 public class HealthCheckerController {
@@ -12,6 +16,19 @@ public class HealthCheckerController {
     @GetMapping
     public ResponseEntity<Object> status () {
         return ResponseEntity.ok().build();
+    }
+    @GetMapping("version")
+    public ResponseEntity<String> version () throws IOException {
+
+        Properties properties = new Properties();
+
+        InputStream inptstm = getClass().getClassLoader().getResourceAsStream("application.yml");
+
+        properties.load(inptstm);
+
+        return ResponseEntity.ok(properties.getProperty("build.name") + " - " + properties.getProperty("build.version"));
+        
+
     }
 
 }
