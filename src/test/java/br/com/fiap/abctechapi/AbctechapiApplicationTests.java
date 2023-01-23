20380769bd941e99de8a2f76e319fdbc538a373e
Danilo Barbosa <danilo_barbosa@hotmail.com>
2023-01-22 21:56:37 -0300

    Service test

--- src/test/java/br/com/fiap/abctechapi/AbctechapiApplicationTests.java
+++ src/test/java/br/com/fiap/abctechapi/AbctechapiApplicationTests.java
@@ -1,13 +1,13 @@
-//package br.com.fiap.abctechapi;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class AbctechapiApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package br.com.fiap.abctechapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbctechapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
