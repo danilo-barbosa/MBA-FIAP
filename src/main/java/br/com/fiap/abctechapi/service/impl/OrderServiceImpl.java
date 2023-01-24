3bfbbb62eb40bc9722fa2f9c72a4e2774ab1adb6
Danilo Barbosa <danilo_barbosa@hotmail.com>
2023-01-23 22:00:47 -0300

    order error handling / test

--- src/main/java/br/com/fiap/abctechapi/service/impl/OrderServiceImpl.java
+++ src/main/java/br/com/fiap/abctechapi/service/impl/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 
 package br.com.fiap.abctechapi.service.impl;
 
+import br.com.fiap.abctechapi.handler.exception.MaxAssistsException;
+import br.com.fiap.abctechapi.handler.exception.MinimumAssistsException;
 import br.com.fiap.abctechapi.model.Assist;
 import br.com.fiap.abctechapi.model.Order;
 import br.com.fiap.abctechapi.repository.AssistRepository;
@@ -13,18 +15,17 @@ import java.util.List;
 
 public class OrderServiceImpl implements OrderService {
 
-    private OrderRepository orderRepository;
-    private AssistRepository assistRepository;
+    private final OrderRepository orderRepository;
+    private final AssistRepository assistRepository;
 
     @Autowired
-    public OrderServiceImpl (
-            OrderRepository orderRepository, AssistRepository assistRepository){
+    public OrderServiceImpl (OrderRepository orderRepository, AssistRepository assistRepository){
         this.orderRepository = orderRepository;
         this.assistRepository = assistRepository;
     }
 
     @Override
-    public void saveOrder(Order order, List<Long> arrayAssists) throws Exception {
+    public void saveOrder(Order order, List<Long> arrayAssists) {
         ArrayList<Assist> assists = new ArrayList<>();
         arrayAssists.forEach( i -> {
             Assist assist = assistRepository.findById(i).orElseThrow();
@@ -33,11 +34,11 @@ public class OrderServiceImpl implements OrderService {
         order.setServices(assists);
 
         if(!order.hasMinAssists()) {
-            throw new Exception();
+            throw new MinimumAssistsException("Invalid Request.","Minimum assists required.");
         }
 
         if(order.exceedsMaxAssists()){
-            throw new Exception();
+            throw new MaxAssistsException("Invalid request.","Maximum assists exceeded.");
         }
 
         orderRepository.save(order);
